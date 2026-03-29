f53921b1aafe69fb399c8b1ac976831823a9987f
Rayvet24 <elias.ivan.rodriguez2004@gmail.com>
2026-03-29 00:31:26 -0300

    Programar el jugador

---
+++ tutorial-godot-2d/player.gd
@@ -0,0 +1,51 @@
+extends Area2D
+
+signal hit
+
+@export var speed = 400
+var screen_size
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2.ZERO #ZERO setea todo en 0
+	#vector de velocidad del jugador local
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed #evita que el 1,1 de velocity haga que vaya mas rápido
+		$AnimatedSprite2D.play() #$ = a get_node
+	else:
+		$AnimatedSprite2D.stop()
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	#clamp() restringe el movimiento en el rango de la pantalla
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "caminar"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "arriba"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+		
+
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+	#set_deferred elimina la colisión asi no se activa varias veces
